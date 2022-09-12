59c4699915e3bd874a1f7ad12c5c3bb80f2115b6
Ubuntu <abeezurrehman7@gmail.com>
2022-09-12 21:13:38 +0000

    Initial Backup

---
+++ app/javascript/application.js
@@ -0,0 +1,10 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+import * as bootstrap from "bootstrap"
+import '@fortawesome/fontawesome-free/js/all';
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+//= require bootstrap-sprockets
